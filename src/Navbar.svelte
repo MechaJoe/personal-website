4a11c3da66ea167feefb48f724acaf55582b802b
Joe Konno <jo3konno@gmail.com>
2023-08-04 14:15:46 -0700

    Make Navbar sticky

--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -1,4 +1,14 @@
-<div class="flex font-thin justify-between">
+<div class="
+  flex
+  sticky
+  top-0
+  font-thin
+  justify-between
+  px-6
+  pt-6
+  bg-white
+  w-full
+  ">
   <div>JOE KONNO</div>
   <a href="https://jkonno-personal-website.s3.amazonaws.com/Joe+Konno+Resume+8-2023.pdf" target="_blank">RESUME</a>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,11 @@
   import Projects from "../Projects.svelte"
   import AboutMe from "../AboutMe.svelte"
   import Footer from "../Footer.svelte"
+    import Navbar from "../Navbar.svelte"
 </script>
 
 <div>
+  <Navbar />
   <Welcome />
   <Projects />
   <AboutMe />
